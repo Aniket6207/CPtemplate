a82a80676a64910fd65d3a6114ec02cf2dd0363e
Aniket6207 <aniketmore6207@gmail.com>
2022-09-13 11:26:49 +0530

    initial commit

---
+++ 1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Aliases
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+ 
+// Constants
+constexpr ll INF = 4e18;
+constexpr ld EPS = 1e-9;
+constexpr ll MOD = 1e9 + 7;
+ 
+// Macros
+#define F first
+#define S second
+#define all(x) begin(x), end(x)
+#define allr(x) rbegin(x), rend(x)
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+
+
+
+
+
+void solve(){
+    
+}
+ 
+ 
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+ 
+ 
+ 
+    return 0;
+}
