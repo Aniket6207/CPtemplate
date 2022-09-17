67d63c21c4e1a119cc0c9610f5795c93e9c2930b
Aniket6207 <aniketmore6207@gmail.com>
2022-09-17 20:00:12 +0530

    made changes

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 // Aliases
 using ll = long long;
 using ull = unsigned long long;
@@ -18,24 +19,93 @@ constexpr ll MOD = 1e9 + 7;
 #define allr(x) rbegin(x), rend(x)
 typedef vector<int> vi;
 typedef pair<int,int> pi;
-#define PB push_back
+#define pb push_back
 #define MP make_pair
-#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define REP(i,a,b) for (int i = a; i < b; i++)
 
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v)
+{
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c)
+{
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
 
-
-
+// Mathematical functions
+int GCD(int a, int b)
+{
+    while (b)
+    {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+ 
+int GCD_extended(int a, int b, int &x, int &y)
+{
+    x = 1, y = 0;
+    int x1 = 0, y1 = 1, a1 = a, b1 = b;
+    while (b1)
+    {
+        int q = a1 / b1;
+        tie(x, x1) = make_tuple(x1, x - q * x1);
+        tie(y, y1) = make_tuple(y1, y - q * y1);
+        tie(a1, b1) = make_tuple(b1, a1 - q * b1);
+    }
+    return a1;
+}
+int LCM(int a, int b)
+{
+    return ((ll)a * b) / GCD(a, b);
+}
+ 
+ll modpow(ll x, ll n, int m = MOD)
+{
+    if (x == 0 && n == 0)
+        return 0; // undefined case
+    ll res = 1;
+    while (n > 0)
+    {
+        if (n % 2)
+            res = (res * x) % m;
+        x = (x * x) % m;
+        n /= 2;
+    }
+    return res;
+}
+ 
+int modinv(int x, int m = MOD)
+{
+    return modpow(x, m - 2, m);
+}
+ 
+mt19937 rng;
+int getRandomNumber(int l, int r)
+{
+    uniform_int_distribution<int> dist(l, r);
+    return dist(rng);
+}
 
 void solve(){
-    
 }
+    
+    
+
  
  
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     int t;
-    cin>>t;
+    t=1;
     while(t--){
         solve();
     }
